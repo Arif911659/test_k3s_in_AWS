8d14a6355e60e4429c96dccb39ad2c531d9b27c6
arifhossain_cse22 <arifaka555@gmail.com>
2024-09-02 01:55:38 +0600

    Add security_group

--- __main__.py
+++ __main__.py
@@ -12,10 +12,11 @@ vpc = aws.ec2.Vpc("my-vpc",
 
 # Add public_subnet
 public_subnet = aws.ec2.Subnet("public-subnet",
-vpc_id = vpc.id,
-cidr_block= "10.10.1.0/24",
-map_public_ip_on_launch=True, availability_zone='ap-southeast-la',
-)
+    vpc_id = vpc.id,
+    cidr_block= "10.10.1.0/24",
+    map_public_ip_on_launch=True, 
+    availability_zone='ap-southeast-la',
+    )
 
 # Add IGW
 igw = aws.ec2.InternetGateway ("igw", vpc_id=vpc.id)
@@ -23,13 +24,41 @@ igw = aws.ec2.InternetGateway ("igw", vpc_id=vpc.id)
 
 # Create Route Table
 route_table = aws.ec2.RouteTable("route-table",
-vpc_id=vpc.id,
-routes=[{
-"cidr_block": "0.0.0.0/0", "gateway_id": igw.id,
-}],
+    vpc_id=vpc.id,
+    routes=[{
+        "cidr_block": "0.0.0.0/0", 
+        "gateway_id": igw.id,
+    }],
 )
 # Associate Route Table with Public Subnet
-rt_assoc_public = aws.ec2.RouteTableAssociation ("rt-assoc-public",
-subnet_id=public_subnet.id,
-route_table_id=route_table.id,
+rt_assoc_public = aws.ec2.RouteTableAssociation("rt-assoc-public",
+    subnet_id=public_subnet.id,
+    route_table_id=route_table.id,
+)
+
+
+# Create Security Group
+security_group = aws.ec2.SecurityGroup("web-secgrp",
+    description='Enable SSH and K3s access',
+    vpc_id=vpc.id,
+    ingress=[
+        {
+            "protocol": "tcp",
+            "from_port": 22,
+            "to_port": 22,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+        {
+            "protocol": "tcp",
+            "from_port": 6443,
+            "to_port": 6443,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+    ],
+    egress=[{
+        "protocol": "-1",
+        "from_port": 0,
+        "to_port": 0,
+        "cidr_blocks": ["0.0.0.0/0"],
+}],
 )
