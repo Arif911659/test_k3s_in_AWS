a79acce53c5cfd8e8a6900521e6b08b646801f20
arifhossain_cse22 <arifaka555@gmail.com>
2024-09-02 01:36:43 +0600

    Add-VPC,Subnet,IGW-for AWS

--- __main__.py
+++ __main__.py
@@ -1,3 +1,21 @@
 """A Python Pulumi program"""
 
 import pulumi
+import pulumi_aws as aws
+
+# Add vpc
+vpc = aws.ec2.Vpc("my-vpc",
+    cidr_block="10.10.0.0/16",
+    enable_dns_hostnames=True,
+    enable_dns_support=True,
+    )
+
+# Add public_subnet
+public_subnet = aws.ec2.Subnet("public-subnet",
+vpc_id = vpc.id,
+cidr_block= "10.10.1.0/24",
+map_public_ip_on_launch=True, availability_zone='ap-southeast-la',
+)
+
+# Add IGW
+igw = aws.ec2.InternetGateway ("igw", vpc_id=vpc.id)
